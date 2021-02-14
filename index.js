3243ee90a8281e0dd60df7589152f7081026ed1f
YaroslavSulyma <yaryksulima@gmail.com>
2021-02-14 13:24:15 +0200

    **Game simulation** Created board 5x5, and made a simple game simulation for checking Gomoku.js

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ game.setChessOf(0, 0, 0);
 
 game.setChessOf(1, 0, 1);
 
+game.setChessOf(0, 1, 0);
+
+game.setChessOf(1, 1, 1);
+
+game.setChessOf(0, 2, 0);
+
+game.setChessOf(1, 1, 2);
+
+game.setChessOf(0, 3, 0);
+
+game.setChessOf(1, 1, 3);
+
+game.setChessOf(0, 4, 0);
+
+game.setChessOf(1, 1, 4);
+
 //  ...
 //
 //  It will return the index of winner
-
-game.setChessOf(0, 0, 4);
