cc70858af18d0d3bc984527b4469dc832e5b55d2
YaroslavSulyma <yaryksulima@gmail.com>
2021-02-18 13:32:54 +0200

    fixed setChess function

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-/*
-game.setChessOf(0, 0, 0);
-
-game.setChessOf(1, 0, 1);
-
-game.setChessOf(0, 1, 0);
-
-game.setChessOf(1, 1, 1);
-
-game.setChessOf(0, 2, 0);
-
-game.setChessOf(1, 1, 2);
-
-game.setChessOf(0, 3, 0);
-
-game.setChessOf(1, 1, 3);
-
-game.setChessOf(0, 4, 0);
-
-game.setChessOf(1, 1, 4);*/
-
 let blessed = require('blessed')
     , contrib = require('blessed-contrib')
 
@@ -28,7 +7,7 @@ let size = 5
 
 let game = new Gomoku(size)
 
-let order = 1 // 1=X, 0=O
+let order = true // 1=X, 0=O
 
 let screen = blessed.screen()
 
@@ -36,16 +15,16 @@ let grid = new contrib.grid({rows: size, cols: size, screen: screen})
 
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-        grid.set(i, j, 4, 4, blessed.box, {
+        grid.set(i, j, 4, 4,  blessed.box, {
             content: '',
             bold: 'bold'
         },)
     }
 }
 
-function onClick(i, j) {
+function setChess(i, j) {
 
-    if (order === 1) {
+    if (order === true) {
         grid.set(i, j, 4, 4, blessed.box, {
             content: 'X',
             bold: 'bold'
@@ -55,12 +34,15 @@ function onClick(i, j) {
             content: 'O',
             bold: 'bold'
         },)
-        order = 1
+        order = false
     }
 
-    game.setChessOf(order, i, j)
-
-    order = 0
+    try {
+        game.setChessOf(order, i, j)
+        order = !order
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 
